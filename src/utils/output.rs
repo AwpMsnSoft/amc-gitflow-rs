f8c68d0682e40d458ba6a78a759a516c31d4808e
MiracleSNeko <miracle.neko@qq.com>
2026-03-17 08:41:01 +0000

    feat(utils): Add colorized console output macros
    Signed-off-by: MiracleSNeko <miracle.neko@qq.com>

---
+++ src/utils/output.rs
@@ -0,0 +1,62 @@
+use colored::{Color, Colorize};
+use std::fmt::Display;
+
+pub(crate) enum Level {
+    Info,
+    Success,
+    Warn,
+    Error,
+    Debug,
+}
+
+pub(crate) fn colored_display<S: Display>(msg: S, level: Level) {
+    let (prefix, color) = match level {
+        Level::Info => ("➥", Color::BrightWhite),
+        Level::Success => ("✔", Color::BrightGreen),
+        Level::Warn => ("⚠", Color::BrightYellow),
+        Level::Error => ("✗", Color::BrightRed),
+        Level::Debug => ("♨", Color::BrightBlack),
+    };
+    let message = format!("{}  {}", prefix, msg).color(color);
+    match level {
+        Level::Info | Level::Success | Level::Debug => println!("{}", message),
+        Level::Warn | Level::Error => eprintln!("{}", message),
+    }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+        $crate::utils::output::colored_display(format!($($arg)*), $crate::utils::output::Level::Info);
+    };
+}
+
+#[macro_export]
+macro_rules! success {
+    ($($arg:tt)*) => {
+        $crate::utils::output::colored_display(format!($($arg)*), $crate::utils::output::Level::Success);
+    };
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        $crate::utils::output::colored_display(format!($($arg)*), $crate::utils::output::Level::Warn);
+    };
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        $crate::utils::output::colored_display(format!($($arg)*), $crate::utils::output::Level::Error);
+    };
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        if cfg!(debug_assertions) || std::env::var("AMC_GITFLOW_DEBUG").is_ok() {
+            $crate::utils::output::colored_display(format!($($arg)*), $crate::utils::output::Level::Debug);
+        }
+    };
+}
